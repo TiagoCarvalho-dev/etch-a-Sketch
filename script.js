a85a76b196aa275bf3688ca2323e791fb8caeb41
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-05-03 23:18:07 -0300

    Created a basic structure for a grid in HTML using js

---
+++ script.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 16; i++) {
+  let newRow = document.createElement('div');
+  newRow.classList.add('row');
+  document.getElementById('gridContainer').appendChild(newRow);
+
+  for (let j = 0; j < 16; j++) {
+    let newColumn = document.createElement('div');
+    newColumn.classList.add('column');
+    newRow.appendChild(newColumn);
+  }
+}
