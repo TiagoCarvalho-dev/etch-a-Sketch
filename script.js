ecd41d91dd81e9dab7582ed68d56bd0ab9357ce6
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-05-06 18:51:18 -0300

    Added function to clear the grid
    Currently the removeExistingGrid function works but it is not being replaced by the new grid

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-for (let i = 0; i < 16; i++) {
-  let newRow = document.createElement('div');
-  newRow.classList.add('row');
-  document.getElementById('gridContainer').appendChild(newRow);
+const gridContainer = document.getElementById('gridContainer');
 
-  for (let j = 0; j < 16; j++) {
-    let newRowSquare = document.createElement('div');
-    newRowSquare.classList.add('rowSquares');
-    newRow.appendChild(newRowSquare);
+let gridSize;
+setGridSize(16);
+
+document.getElementById('changeGridSizeButton').addEventListener('click', () => {
+  gridSize = prompt('Type the amount of squares per side you want the new grid to have:');
+  removeExistingGrid();
+  setGridSize(gridSize);
+});
+
+function setGridSize(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    let newRow = document.createElement('div');
+    newRow.classList.add('row');
+    gridContainer.appendChild(newRow);
+  
+    for (let j = 0; j < gridSize; j++) {
+      let newRowSquare = document.createElement('div');
+      newRowSquare.classList.add('rowSquares');
+      newRow.appendChild(newRowSquare);
+    }
+  }
+}
+
+const rows = document.querySelectorAll('.row');
+
+function removeExistingGrid() {
+  rows.forEach((e) => {
+    for(let i = 0; i < rows.length; i++) {
+      e.removeChild(e.lastChild);
+    }
+  })
+  for(let j = 0; j < rowSquares.length; j++) {
+    gridContainer.removeChild(gridContainer.lastChild);
   }
 }
 
 const rowSquares = document.querySelectorAll('.rowSquares');
+
 rowSquares.forEach((e) => {
   e.addEventListener('mouseover', () => {
     e.setAttribute('style', 'background-color: blue');
