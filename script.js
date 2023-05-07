5f931ee4b8d928426727a027d422b1edb509a5be
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-05-07 11:51:04 -0300

    Fixed issue with replacing the grid with the new one
    Before the fix, the grid would only change once and then issue a error. This was due to the variable being declared outside the function and it's value not being replace for the new grid.

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const gridContainer = document.getElementById('gridContainer');
 let gridSize;
 setGridSize(16);
 
+const rowSquares = document.querySelectorAll('.rowSquares');
+
+rowSquares.forEach((e) => {
+  e.addEventListener('mouseover', () => {
+    e.setAttribute('style', 'background-color: blue');
+  });
+});
+
 document.getElementById('changeGridSizeButton').addEventListener('click', () => {
   gridSize = prompt('Type the amount of squares per side you want the new grid to have:');
   removeExistingGrid();
@@ -23,23 +31,15 @@ function setGridSize(gridSize) {
   }
 }
 
-const rows = document.querySelectorAll('.row');
-
 function removeExistingGrid() {
+  const rows = document.querySelectorAll('.row');
+  
   rows.forEach((e) => {
     for(let i = 0; i < rows.length; i++) {
       e.removeChild(e.lastChild);
     }
-  })
-  for(let j = 0; j < rowSquares.length; j++) {
-    gridContainer.removeChild(gridContainer.lastChild);
-  }
-}
-
-const rowSquares = document.querySelectorAll('.rowSquares');
-
-rowSquares.forEach((e) => {
-  e.addEventListener('mouseover', () => {
-    e.setAttribute('style', 'background-color: blue');
   });
-});
+    for(k = 0; k < rows.length; k++){
+      gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
